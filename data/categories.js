4e88dcaacca4f2c3e864dd9b2e64725ed8301769
Katelinn Jones <katiejonesyo@gmail.com>
2020-11-04 23:07:48 -0800

    starting join

---
+++ data/categories.js
@@ -0,0 +1,11 @@
+module.exports = [
+{
+     name:'citrus'
+},
+{
+     name:'sweet'
+},
+{
+     name:'sour'
+}
+];
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,5 +1,6 @@
 const client = require('../lib/client');
 const lacroixs = require('./lacroixs.js');
+const categories = require('./categories.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -20,16 +21,26 @@ async function run() {
         [user.email, user.hash]);
       })
     );
+
+    await Promise.all(
+      categories.map(category => {
+        return client.query(`
+                    INSERT INTO categories (name)
+                    VALUES ($1);
+                `,
+        [category.name]);
+      })
+    );
       
     const user = users[0].rows[0];
 
     await Promise.all(
       lacroixs.map(lacroixs => {
         return client.query(`
-                    INSERT INTO lacroixs (id, name, cool_factor, category, crisp)
+                    INSERT INTO lacroixs (category_id, name, cool_factor, category, crisp)
                     VALUES ($1, $2, $3, $4, $5);
                 `,
-        [lacroixs.id, lacroixs.name, lacroixs.cool_factor, lacroixs.category, lacroixs.crisp]);
+        [lacroixs.category_id, lacroixs.name, lacroixs.cool_factor, lacroixs.category, lacroixs.crisp]);
       })
     );
     
